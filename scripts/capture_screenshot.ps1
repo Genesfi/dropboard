77c74fa67112e1c13a953e8c27676b81ebc58667
Genesfi <gusti.nsfians@gmail.com>
2026-09-19 23:05:35 +0800

    chore: sanitize script path

--- scripts/capture_screenshot.ps1
+++ scripts/capture_screenshot.ps1
@@ -5,7 +5,7 @@ $screen = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds
 $bitmap = New-Object System.Drawing.Bitmap $screen.Width, $screen.Height
 $graphics = [System.Drawing.Graphics]::FromImage($bitmap)
 $graphics.CopyFromScreen($screen.Location, [System.Drawing.Point]::Empty, $screen.Size)
-$outPath = 'C:\Users\Gusti N\.gemini\antigravity-ide\brain\458fe9ab-4c84-4bd4-a572-aa54f3266268\screenshot_dropboard.png'
+$outPath = Join-Path $PSScriptRoot "screenshot_dropboard.png"
 $bitmap.Save($outPath, [System.Drawing.Imaging.ImageFormat]::Png)
 $graphics.Dispose()
 $bitmap.Dispose()
